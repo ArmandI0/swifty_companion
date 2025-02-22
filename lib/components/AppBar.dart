02a317fe9f9d58179ef160abaeada4c6340c3fde
Your Name <you@example.com>
2025-02-22 14:22:55 +0100

    prbleme sur les boutons

--- lib/components/AppBar.dart
+++ lib/components/AppBar.dart
@@ -2,27 +2,22 @@ import 'package:flutter/material.dart';
 export 'AppBar.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-
   const MyAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Row(
-        // On utilise Row dans le title
-        children: [
-          Expanded(
-            // Le texte prend l'espace minimal nécessaire
-            child: Text('Swifty Companion'),
-          ),
-        ],
-      ),
-      actions: [
-        // Les boutons restent dans actions
-        IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
-      ],
+      leading:
+          Navigator.canPop(context)
+              ? IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+              : null,
+      title: Row(children: [Expanded(child: Text('Swifty Companion'))]),
     );
   }
+
   @override
   Size get preferredSize => Size.fromHeight(56.0);
 }
